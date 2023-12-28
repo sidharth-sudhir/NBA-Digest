9addf44c471ee467761345b36401fac7f91f0184
Sidharth Sudhir <sidharth2ndacc@gmail.com>
2023-12-27 23:05:19 -0800

    initial framework

---
+++ frontend/nba-digest/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ScoresComponent from './components/ScoresComponent'; // Adjust the path accordingly
+
+function App() {
+  return (
+    <div className="App">
+      <ScoresComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/nba-digest/src/components/ScoresComponent.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function ScoresComponent() {
+  const [scores, setScores] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/scores') // Fetch data from backend endpoint
+      .then(response => {
+        setScores(response.data); // Set fetched data to state
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Scores</h2>
+      <ul>
+        {scores.map(score => (
+          <li key={score.id}>
+            {score.homeTeam} {score.homeScore} - {score.awayScore} {score.awayTeam}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ScoresComponent;
